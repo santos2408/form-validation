d54450a9632b7a9821cc8c227d1082ce3546be7b
Roger Santos <roger.santos36@gmail.com>
2022-03-17 16:05:02 -0300

    refatorada variavel do elemento p

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 // VALIDANDO CARACTERES INSERIDOS NO INPUT
 const inputUsername = document.querySelector('#username')
-const p = document.createElement('p')
+const paragraphFeedback = document.createElement('p')
+
 
 inputUsername.addEventListener('keyup', event => {
-   const username = event.target.value 
+   const inputValue = event.target.value 
    const usernameRegex = /^[a-zA-Z]{6,}$/
-   const isAValidUsername = usernameRegex.test(username)
+   const isAValidUsername = usernameRegex.test(inputValue)   
 
    if (!isAValidUsername) {
-      p.setAttribute('class', 'username-help-feedback')
-      p.innerHTML = 'O valor deve conter no mínimo 6 caracteres, com apenas letras maiúsculas e/ou minúsculas.'
+      paragraphFeedback.setAttribute('class', 'username-help-feedback')
+      paragraphFeedback.innerHTML = 'O valor deve conter no mínimo 6 caracteres, com apenas letras maiúsculas e/ou minúsculas.'
       inputUsername.style.borderColor = '#E8B635'
-      inputUsername.insertAdjacentElement('afterend', p)
+      inputUsername.insertAdjacentElement('afterend', paragraphFeedback)
       return
    }
 
-   p.setAttribute('class', 'username-success-feedback')
-   p.textContent = 'Esse nome de usuário é válido!'
+   paragraphFeedback.setAttribute('class', 'username-success-feedback')
+   paragraphFeedback.textContent = 'Esse nome de usuário é válido!'
    inputUsername.style.borderColor = '#6EC080'
-   inputUsername.insertAdjacentElement('afterend', p)
+   inputUsername.insertAdjacentElement('afterend', paragraphFeedback)
 })
